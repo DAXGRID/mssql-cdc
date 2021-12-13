8d6db497429e648c6f154a6273211b041e7cd7cd
Rune Nielsen <runenielsen@runbox.com>
2021-12-13 14:25:59 +0100

    updates example with reading changes

--- examples/Example/Program.cs
+++ examples/Example/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using MsSqlCdc;
@@ -9,23 +13,68 @@ namespace Example;
 
 public class Program
 {
-    public static async Task Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Starting to listen");
+        var connectionString = CreateConnectionString();
+        var pollingIntervalMs = 100;
+        var tables = new List<string> { "dbo_Employee" };
+        var cdcCancellation = new CancellationTokenSource();
+        var cdcCancellationToken = cdcCancellation.Token;
 
-        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-        builder.DataSource = "localhost";
-        builder.UserID = "sa";
-        builder.Password = "myAwesomePassword1";
-        builder.InitialCatalog = "TestDb";
-        builder.Encrypt = false;
+        var myChannel = Channel.CreateUnbounded<IReadOnlyCollection<ChangeData<dynamic>>>();
+        _ = Task.Factory.StartNew(async () =>
+        {
+            long lowBoundLsn = await GetStartLsn(connectionString);
+            while (true)
+            {
+                if (cdcCancellationToken.IsCancellationRequested)
+                {
+                    // We mark the channel as completed to notify that all consumers should
+                    // read the last elements and stop.
+                    myChannel.Writer.Complete();
+                    break;
+                }
 
-        var connectionString = builder.ConnectionString;
-        using var connection = new SqlConnection(connectionString);
-        await connection.OpenAsync();
+                using var connection = new SqlConnection(connectionString);
+                try
+                {
+                    await connection.OpenAsync();
 
-        var minLsn = await Cdc.GetMinLsn(connection, "dbo_Employee");
-        var maxLsn = await Cdc.GetMaxLsn(connection);
+                    var highBoundLsn = await Cdc.GetMaxLsn(connection);
+
+                    if (lowBoundLsn <= highBoundLsn)
+                    {
+                        Console.WriteLine($"Polling with from '{lowBoundLsn}' to '{highBoundLsn}");
+
+                        var changes = new List<ChangeData<dynamic>>();
+                        foreach (var table in tables)
+                        {
+                            var changeSets = await Cdc.GetAllChanges(connection, table, lowBoundLsn, highBoundLsn);
+                            changes.AddRange(changeSets);
+                        }
+
+                        var orderedChanges = changes.OrderBy(x => x.SequenceValue).ToList();
+                        await myChannel.Writer.WriteAsync(orderedChanges);
+
+                        lowBoundLsn = await Cdc.GetNextLsn(connection, highBoundLsn);
+                    }
+                    else
+                    {
+                        // No changes
+                        Console.WriteLine($"No changes since last poll '{lowBoundLsn}'");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    await connection.CloseAsync();
+                    await Task.Delay(pollingIntervalMs);
+                }
+            }
+        }, cdcCancellationToken);
 
         var options = new JsonSerializerOptions
         {
@@ -36,9 +85,35 @@ public class Program
             }
         };
 
-        var changeSets = await Cdc.GetAllChanges(connection, "dbo_Employee", minLsn, maxLsn);
+        _ = Task.Factory.StartNew(async () =>
+        {
+            await foreach (var changes in myChannel.Reader.ReadAllAsync())
+            {
+                var changeDataJson = JsonSerializer.Serialize(changes, options);
+                Console.WriteLine(changeDataJson + "\n");
+            }
+        });
 
-        var result = JsonSerializer.Serialize(changeSets, options);
-        Console.WriteLine(result);
+        Console.ReadKey();
+        cdcCancellation.Cancel();
+    }
+
+    private static async Task<long> GetStartLsn(string connectionString)
+    {
+        using var connection = new SqlConnection(connectionString);
+        await connection.OpenAsync();
+        var currentMaxLsn = await Cdc.GetMaxLsn(connection);
+        return await Cdc.GetNextLsn(connection, currentMaxLsn);
+    }
+
+    private static string CreateConnectionString()
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        builder.DataSource = "localhost";
+        builder.UserID = "sa";
+        builder.Password = "myAwesomePassword1";
+        builder.InitialCatalog = "TestDb";
+        builder.Encrypt = false;
+        return builder.ConnectionString;
     }
 }
