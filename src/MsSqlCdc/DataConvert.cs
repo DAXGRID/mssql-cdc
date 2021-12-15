d552f0552502311fc7aba96dc3f4d9710ea7ef4b
Rune Nielsen <runenielsen@runbox.com>
2021-12-15 22:37:01 +0100

    adds test suite for data-convert (#9)

--- src/MsSqlCdc/DataConvert.cs
+++ src/MsSqlCdc/DataConvert.cs
@@ -11,17 +11,21 @@ public static class DataConvert
     /// <summary>
     /// Converts a a colection of columns represented as Tuple<string, object> to ChangeData<dynamic> representation.
     /// </summary>
-    /// <param name="column">List of tuples with Item1 being the name column and Item2 being the column value</param>
+    /// <param name="columnFields">List of tuples with Item1 being the name column and Item2 being the column value</param>
     /// <param name="captureInstance">The tablename of the column.</param>
     /// <returns>Returns the CDC column as a ChangeData record.</returns>
-    public static ChangeData<dynamic> ConvertCdcColumn(List<Tuple<string, object>> column, string captureInstance)
+    /// <exception cref="Exception"></exception>
+    public static ChangeData<dynamic> ConvertCdcColumn(List<Tuple<string, object>> columnFields, string captureInstance)
     {
-        var startLsn = ConvertBinaryLsn((byte[])column[0].Item2);
-        var seqVal = ConvertBinaryLsn((byte[])column[1].Item2);
-        var operation = ConvertIntOperation((int)column[2].Item2);
-        var updateMask = Encoding.UTF8.GetString((byte[])column[3].Item2);
+        if (columnFields.Count < 3)
+            throw new Exception($"Count of column fields should be 4 or greater, instead got '{columnFields.Count}'.");
 
-        var body = column.Skip(4)
+        var startLsn = ConvertBinaryLsn((byte[])columnFields[0].Item2);
+        var seqVal = ConvertBinaryLsn((byte[])columnFields[1].Item2);
+        var operation = ConvertIntOperation((int)columnFields[2].Item2);
+        var updateMask = Encoding.UTF8.GetString((byte[])columnFields[3].Item2);
+
+        var body = columnFields.Skip(4)
             .Aggregate(new ExpandoObject() as IDictionary<string, object>,
                        (acc, x) => { acc[x.Item1] = x.Item2; return acc; }) as dynamic;
 
---
+++ test/MsSqlCdc.Tests/DataConvertTest.cs
@@ -0,0 +1,105 @@
+using Xunit;
+using FluentAssertions;
+using static FluentAssertions.FluentActions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using FluentAssertions.Execution;
+
+namespace MsSqlCdc.Tests;
+
+public class DataConverTest
+{
+    [Theory]
+    [InlineData(25000L, 25002L, Operation.AfterUpdate, "ABB", 10, "Rune", 20000.00)]
+    [InlineData(25L, 25202L, Operation.BeforeUpdate, "DSDFS", 2, "Simon", 0.00)]
+    [InlineData(250000000L, 250021L, Operation.Delete, "DFS", 3, "Foo", 1000000000.00)]
+    [InlineData(0L, 2L, Operation.Insert, "DFS", 3, "John", 1000000000.00)]
+    public void ConvertCdcColumn_ShouldReturnChangeData_OnValidInput(
+        long startLsn,
+        long seqVal,
+        Operation operation,
+        string updateMask,
+        int id,
+        string name,
+        double salary)
+    {
+        var captureInstance = "dbo_Employee";
+        var columnFields = new List<Tuple<string, object>>
+        {
+            new Tuple<string, object>("__$start_lsn", BitConverter.GetBytes(startLsn).Reverse().ToArray()),
+            new Tuple<string, object>("__$seqval", BitConverter.GetBytes(seqVal).Reverse().ToArray()),
+            new Tuple<string, object>("__$operation", (int)operation),
+            new Tuple<string, object>("__$update_mask", Encoding.ASCII.GetBytes(updateMask)),
+            new Tuple<string, object>("Id", id),
+            new Tuple<string, object>("Name", name),
+            new Tuple<string, object>("Salary", salary),
+        };
+
+        var changeData = new ChangeData<dynamic>(
+            startLsn,
+            seqVal,
+            operation,
+            updateMask,
+            "dbo_Employee",
+            new
+            {
+                Id = id,
+                Name = name,
+                Salary = salary,
+            }
+        );
+
+        var result = DataConvert.ConvertCdcColumn(columnFields, captureInstance);
+
+        using (var scope = new AssertionScope())
+        {
+            Assert.True(result.Body.Id == changeData.Body.Id);
+            Assert.True(result.Body.Name == changeData.Body.Name);
+            Assert.True(result.Body.Salary == changeData.Body.Salary);
+            result.StartLineSequenceNumber.Should().Be(changeData.StartLineSequenceNumber);
+            result.SequenceValue.Should().Be(changeData.SequenceValue);
+            result.Operation.Should().Be(changeData.Operation);
+            result.UpdateMask.Should().Be(changeData.UpdateMask);
+        }
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void ConvertCdcColumn_ShouldThrowException_OnColumnFieldsBeingLessThanFour(int columnFieldsCount)
+    {
+        var captureInstance = "dbo_Employee";
+
+        var columnFields = Enumerable.Range(0, columnFieldsCount)
+            .Select(x => new Tuple<string, object>(x.ToString(), x)).ToList();
+
+        Invoking(() => DataConvert.ConvertCdcColumn(columnFields, captureInstance)).Should().Throw<Exception>();
+    }
+
+    [Theory]
+    [InlineData(1, Operation.Delete)]
+    [InlineData(2, Operation.Insert)]
+    [InlineData(3, Operation.BeforeUpdate)]
+    [InlineData(4, Operation.AfterUpdate)]
+    public void ConvertIntOperation_ShouldReturnCorrectEnumConvertion(int input, Operation expected)
+    {
+        var operation = DataConvert.ConvertIntOperation(input);
+        operation.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-10)]
+    [InlineData(0)]
+    [InlineData(100)]
+    [InlineData(int.MinValue)]
+    [InlineData(int.MaxValue)]
+    public void ConvertIntOperation_ShouldThrowException_OnInvalidIntRepresentation(int input)
+    {
+        Invoking(() => DataConvert.ConvertIntOperation(input)).Should().Throw<ArgumentException>();
+    }
+}
